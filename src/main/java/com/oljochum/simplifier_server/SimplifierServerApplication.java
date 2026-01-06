457480a22f2554152fcf607459a415371f064aeb
Oliver Jochum <oliver.jochum@lufthansa-group.com>
2026-01-06 19:25:54 +0100

    create spring project

---
+++ src/main/java/com/oljochum/simplifier_server/SimplifierServerApplication.java
@@ -0,0 +1,13 @@
+package com.oljochum.simplifier_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifierServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifierServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oljochum/simplifier_server/SimplifierServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oljochum.simplifier_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifierServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
