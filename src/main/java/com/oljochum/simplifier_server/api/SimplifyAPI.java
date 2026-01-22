8e614cb0b699272fea0dd5519a97a715be68a3eb
Oliver Jochum <oliver.jochum@yahoo.com>
2026-01-22 13:40:54 +0100

    Clean up simplify req mapping

--- src/main/java/com/oljochum/simplifier_server/api/SimplifyAPI.java
+++ src/main/java/com/oljochum/simplifier_server/api/SimplifyAPI.java
@@ -3,6 +3,7 @@ package com.oljochum.simplifier_server.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oljochum.simplifier_server.simplify.SimplifyController;
@@ -10,26 +11,27 @@ import com.oljochum.simplifier_server.simplify.SimplifyRequestDTO;
 import com.oljochum.simplifier_server.simplify.SynonymRequestDTO;
 
 @RestController
+@RequestMapping("/api/simplify/")
 public class SimplifyAPI {
     @Autowired
     private SimplifyController simplifyController;
 
-    @PostMapping("/api/simplify/generate_text")
+    @PostMapping("generate_text")
     public String postSimplifyGenerateText(@RequestBody SimplifyRequestDTO req) {
         return simplifyController.handleSimplifyGenerateText(req);
     }
 
-    @PostMapping("/api/simplify/sentence_simplify")
+    @PostMapping("sentence_simplify")
     public String postSimplifySentenceSimplify(@RequestBody SimplifyRequestDTO req) {
         return simplifyController.handleSimplifySentenceSimplify(req);
     }
 
-    @PostMapping("/api/simplify/sentence_suggest")
+    @PostMapping("sentence_suggest")
     public String postSimplifySentenceSuggest(@RequestBody SimplifyRequestDTO req) {
         return simplifyController.handleSimplifySentenceSuggest(req);
     }
 
-    @PostMapping("/api/simplify/synonyms")
+    @PostMapping("synonyms")
     public String postSimplifySynonyms(@RequestBody SynonymRequestDTO req) {
         return simplifyController.handleSimplifySynonyms(req);
     }
